528968d4dc6fdcf5b0cb977f6964234cf7210b89
rocky <dineshofficialkd@gmail.com>
2026-09-07 22:12:18 +0530

    Add litellm fallback

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv
 from litellm import completion
 
 load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-model = os.getenv("model","gemini/gemini-2.5-flash")
+#model = os.getenv("model","gemini/gemini-2.5-flash")
+model ="gemini/gemini-1.5-flash"
+
+# models = [
+#     "primary-model",
+#     "backup-model"
+# ]
 response = completion(
     model=model,
     messages=[
@@ -13,7 +18,10 @@ response = completion(
             "content":"write a 2 sentace mindset to become million simple action but huge result"
         }
     ],
-    api_key=os.getenv("GOOGLE_API_KEY")
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    #add fall back 4.
+    fallbacks=["gemini/gemini-2.5-flash-lite"]
+
 )
 
 print(response.choices[0].message.content)
